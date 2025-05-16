2c845e70dc80f387355ad775ce83f33c0af523b6
0xySan <ethtaquet@gmail.com>
2025-05-16 16:07:26 +0200

    added example env and pages for static website

--- srcs/requirements/nginx/site/main.js
+++ srcs/requirements/nginx/site/main.js
@@ -1,3 +1,20 @@
+function goToPage2() {
+  window.location.href = 'page2.html'; // Change this to your target HTML file
+}
+
+function goToPage3() {
+  window.location.href = 'page3.html'; // Change this to your target HTML file
+}
+
+function goToPage1() {
+  window.location.href = 'rickroll.html'; // Change this to your target HTML file
+}
+
+function goToIndex() {
+  window.location.href = 'index.html'; // Change this to your target HTML file
+}
+
+
 const themeSelector = document.getElementById('themeSelector');
 
 themeSelector.addEventListener('change', function() {
